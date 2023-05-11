c4ad407630bebfdeed1d7ba4ef032a3371f2c7e7
FoPeJLb <95053089+FoPeJLb@users.noreply.github.com>
2023-05-12 01:25:28 +0300

    Create launch.sh

---
+++ dotfiles/polybar/launch.sh
@@ -0,0 +1,14 @@
+# Terminate already running bar instances
+killall -q polybar
+
+# Wait until the processes have been shut down
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+
+# Launch bar
+if type "xrandr"; then
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    MONITOR=$m polybar -c ~/.config/polybar/config &
+  done
+else
+  polybar -c ~/.config/polybar/config &
+fi
