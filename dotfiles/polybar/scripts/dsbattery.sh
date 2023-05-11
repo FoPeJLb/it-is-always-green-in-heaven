065120ffc818cee51264d70e7cbac84ffc8d5e71
FoPeJLb <95053089+FoPeJLb@users.noreply.github.com>
2023-05-12 02:16:20 +0300

    Update dsbattery.sh

--- dotfiles/polybar/scripts/dsbattery.sh
+++ dotfiles/polybar/scripts/dsbattery.sh
@@ -1,3 +1,5 @@
+# Модуль, который показывает уровень заряда подключенного геймпада 
+# (Для того,чтобы узнать id необходимо прописать upower -d, и вставить нужное вместо "ps-controller-battery-a0:ab:51:89:94:65" из строки "native-path")
 if [ -e /sys/class/power_supply/ps-controller-battery-a0:ab:51:89:94:65/capacity ]
 then
     cat /sys/class/power_supply/ps-controller-battery-a0:ab:51:89:94:65/capacity | awk '{print"  "$0"%"}'
